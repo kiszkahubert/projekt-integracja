aece0046d8c867e1e88f255767ec71df29ad4491
Hubert <kiszkahub@gmail.com>
2025-05-23 00:26:37 +0200

    init

---
+++ src/main/java/com/kiszka/integracja/IntegracjaApplication.java
@@ -0,0 +1,12 @@
+package com.kiszka.integracja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracjaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiszka/integracja/IntegracjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiszka.integracja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
